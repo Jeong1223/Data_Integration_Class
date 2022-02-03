bb3ac61bae5b320aa6d1e0429beda041b74d2c9b
Jeong Eun Jang <71644016+Jeong1223@users.noreply.github.com>
2022-02-03 14:29:32 -0400

    Update PL_SQL.sql

--- PL_SQL.sql
+++ PL_SQL.sql
@@ -49,3 +49,59 @@ BEGIN
  
   DBMS_OUTPUT.PUT_LINE(MY_FAVORITE_FOOD);
 END;
+
+--LOOP 1
+--Different approach
+SET SERVEROUP ON
+DECLARE
+  L_FNAME CUSTOMER.C_FIRST%TYPE;
+  L_MI CUSTOMER.C_MI%TYPE;
+  L_LNAME CUSTOMER.C_LAST%TYPE;
+  L_COUNTER NUMBER := 0;
+
+BEGIN
+  LOOP 
+      --L_COUNTER := C_ID;
+      L_COUNTER := L_COUNTER +1;
+      EXIT WHEN L_COUNTER  > 6;
+      
+      SELECT C_FIRST, C_MI, C_LAST
+      INTO L_FNAME, L_MI, L_LNAME
+      FROM CUSTOMER
+      WHERE C_ID = L_COUNTER;
+      
+      DBMS_OUTPUT.PUT_LINE('CUSTOMER NAME: '|| L_COUNTER || ' ' || L_FNAME || ' ' || L_MI || ' ' || L_LNAME|| L_MI );
+  END LOOP;
+--control resumes here after EXIT
+--DBMS_OUTPUT.PUT_LINE( 'WE HAVE: ' || l_counter || ' CUSTOMERS' );
+
+END;
+
+
+--LOOP2
+DECLARE
+  L_FNAME VARCHAR(30);
+  L_MI CHAR(1);
+  L_LNAME VARCHAR(30);
+  L_COUNTER NUMBER := 0;
+
+BEGIN
+
+  FOR C IN (SELECT C_FIRST, C_MI, C_LAST
+            FROM CUSTOMER
+            )
+
+LOOP
+
+    L_COUNTER := L_COUNTER + 1;
+
+    --EXIT WHEN L_COUNTER > 7;
+    
+    DBMS_OUTPUT.PUT_LINE('CUSTOMER NAME: '|| L_COUNTER || ' ' || C.C_FIRST || ' ' || C.C_MI || ' ' || C.C_LAST|| C.C_MI );
+   
+END LOOP;
+
+--control resumes here after EXIT
+DBMS_OUTPUT.PUT_LINE( 'WE HAVE: ' || l_counter || ' CUSTOMERS' );
+
+END;
